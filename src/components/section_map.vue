86dd2b364b789bd0ff2bda2703747bc3a27a3ba4
Ke77er <Jonatan.keller@hotmail.com>
2023-12-24 04:16:02 -0300

    aleluia o mapa deu certo

--- src/components/section_map.vue
+++ src/components/section_map.vue
@@ -12,43 +12,276 @@ export default {
   },
   methods: {
     async initMapbox() {
+      const response = await fetch(
+        'https://api.maptiler.com/maps/hybrid/style.json?key=uB5DVeztu62syAixuO9B'
+      );
       try {
-        const response = await fetch(
-          "https://api.maptiler.com/maps/hybrid/style.json?key=uB5DVeztu62syAixuO9B"
-        );
-
-        if (!response.ok) {
-          throw Error(response.statusText);
-        }
-
+        if (!response.ok) throw Error(response.statusText);
         const style = await response.json();
         const map = new mapboxgl.Map({
-          container: "map",
+          container: 'map',
           style: style,
-          zoom: 10,
+          zoom: 7,
           center: [-48.845116, -26.303207],
         });
-
-        // Dados dos marcadores
-        const markersData = [
-          { color: "#FF0000", coordinates: [-48.845116, -26.303207] },
-          // Adicione mais marcadores conforme necessário
-        ];
-
-        // Adiciona os marcadores ao mapa
-        markersData.forEach(markerData => {
-          const marker = new mapboxgl.Marker({ color: markerData.color })
-            .setLngLat(markerData.coordinates)
-            .addTo(map);
-        });
-
+        this.initMap(map);
       } catch (error) {
         console.error({ error });
-        const errorWrap = document.createElement("div");
-        errorWrap.innerHTML =
-          "<h1>Uh Oh, houve um erro ao carregar o mapa 😭</h1>";
+        const errorWrap = document.createElement('div');
+        errorWrap.innerHTML = "<h1>Uh Oh, houve um erro ao carregar o mapa 😭</h1>";
         document.body.appendChild(errorWrap);
       }
+    },
+    initMap(map) {
+      const bounds = new mapboxgl.LngLatBounds();
+      const markers = [];
+
+      function getComputedTranslateXY(obj) {
+        const transArr = [];
+        if (!window.getComputedStyle) return;
+        const style = getComputedStyle(obj),
+          transform = style.transform || style.webkitTransform || style.mozTransform;
+        let mat = transform.match(/^matrix3d\((.+)\)$/);
+        if (mat) return parseFloat(mat[1].split(', ')[13]);
+        mat = transform.match(/^matrix\((.+)\)$/);
+        mat ? transArr.push(parseFloat(mat[1].split(', ')[4])) : 0;
+        mat ? transArr.push(parseFloat(mat[1].split(', ')[5])) : 0;
+        return transArr;
+      }
+
+      function getWindowXYSize() {
+        const windowXY = [];
+        const w = window;
+        const d = document;
+        const e = d.documentElement;
+        const g = d.getElementsByTagName('body')[0];
+        const x = w.innerWidth || e.clientWidth || g.clientWidth;
+        const y = w.innerHeight || e.clientHeight || g.clientHeight;
+        windowXY.push(x, y);
+        return windowXY;
+      }
+
+      const iconCity = `
+    <path class="e-marker__icon e-marker__icon--city" d="M41.1,68.7V58.5L36,53.4l-5.1,5.1v3.4H20.8v23.7h30.5V68.7H41.1z M27.5,82.2h-3.4v-3.4h3.4V82.2z M27.5,75.4h-3.4
+	V72h3.4V75.4z M27.5,68.7h-3.4v-3.4h3.4V68.7z M37.7,82.2h-3.4v-3.4h3.4V82.2z M37.7,75.4h-3.4V72h3.4V75.4z M37.7,68.7h-3.4v-3.4
+	h3.4V68.7z M37.7,61.9h-3.4v-3.4h3.4V61.9z M47.9,82.2h-3.4v-3.4h3.4V82.2z M47.9,75.4h-3.4V72h3.4V75.4z"/>
+`;
+
+      const geojson = {
+        "type": "FeatureCollection",
+        "features": [
+          {
+            "type": "Feature",
+            "properties": {
+              "title": "Oaxaca City",
+              "imageUrl": "https://c1.staticflickr.com/5/4357/36172707494_2b8ea05f04_q.jpg",
+              "type": "city",
+            },
+            "geometry": {
+              "type": "Point",
+              "coordinates": [
+              -48.845116,
+              -26.303207,
+              ]
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "title": "City",
+              "imageUrl": "https://c1.staticflickr.com/5/4357/36172707494_2b8ea05f04_q.jpg",
+              "type": "city",
+            },
+            "geometry": {
+              "type": "Point",
+              "coordinates": [
+              -48.733116,
+              -26.303207,
+              ]
+            }
+          }
+        ]
+      };
+
+      function renderMarker(marker) {
+    const { title, imageUrl, type } = marker.properties;
+    const titleArr = title.split(' ');
+    const titleLast = titleArr.slice(Math.ceil(titleArr.length / 2), titleArr.length);
+    const titleFirst = titleArr.slice(0, Math.ceil(titleArr.length / 2));
+    let currentIcon = '';
+    
+    switch(type) {
+        case 'city':
+            currentIcon = iconCity;
+            break;
+        default: 
+            currentIcon = '';
+    };
+  
+    return `
+        <div>
+            <svg class="e-marker" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 72 130.7" width="36">
+                <defs>
+                    <clipPath id="circle">
+                        <path d="M36,97.4c15,0,27.3-12.2,27.3-27.3c0-15-12.2-27.3-27.3-27.3S8.7,55.1,8.7,70.2S21,97.4,36,97.4z"/>
+                    </clipPath>
+                </defs>
+                <path class="e-marker__marker" d="M60.7,45.4C54.1,38.8,45.3,35.2,36,35.2c-9.3,0-18.1,3.6-24.7,10.3C4.6,52,1,60.8,1,70.2c0,6.3,1.5,11.6,4.6,16.7
+      C8.4,91.3,12.1,95,16,98.9c7.3,7.2,15.5,15.4,19,30.5c0.1,0.5,0.5,0.8,1,0.8s0.9-0.3,1-0.8c3.5-15.1,11.7-23.3,19-30.5
+      c3.9-3.9,7.6-7.6,10.4-12.1c3.1-5.1,4.6-10.3,4.6-16.7C71,60.8,67.4,52,60.7,45.4z M36,97.4c-15,0-27.3-12.2-27.3-27.3
+      S21,42.9,36,42.9c15,0,27.3,12.2,27.3,27.3C63.3,85.2,51,97.4,36,97.4z"/>
+          <path class="e-marker__circle" d="M36,97.4c15,0,27.3-12.2,27.3-27.3c0-15-12.2-27.3-27.3-27.3S8.7,55.1,8.7,70.2S21,97.4,36,97.4z"/>
+                ${currentIcon}
+                <image class="e-marker__image" width="100%" height="100%" clip-path="url(#circle)" xlink:href="${imageUrl}" />
+                <text class="e-marker__text" transform="matrix(1 0 0 1 0 13.9998)">
+                    <tspan x="50%" y="0" >${titleFirst.join(' ')}</tspan>
+                    <tspan x="50%" y="13" >${titleLast.join(' ')}</tspan>      
+                </text>
+            </svg>
+        </div>
+    `;
+}
+
+      function offSetMarker(marker, markerGrowSize, map) {
+    // Set the max width and height of the marker and shrink it a bit by multiplying with 0.x. This is to compensate for padding around the marker
+    const markerMaxWidth = (marker.offsetWidth * markerGrowSize) * 0.55;
+    const markerMaxHeight = (marker.offsetHeight * markerGrowSize) * 0.7;
+    const markerOffSetX = getComputedTranslateXY(marker)[0];
+    const markerOffSetY = getComputedTranslateXY(marker)[1];
+    if (
+        markerOffSetY < markerMaxHeight
+        || markerOffSetX < markerMaxWidth
+        || (getWindowXYSize()[0] - markerOffSetX) < (markerMaxWidth + marker.offsetWidth)
+    ) {
+        let offSetY = 0;
+        let offSetX = 0;
+
+        if (markerOffSetY < markerMaxHeight) {            
+            offSetY = markerOffSetY - markerMaxHeight;
+        }
+
+        if (markerOffSetX < markerMaxWidth) {
+            offSetX = markerOffSetX - markerMaxWidth;
+        }
+        // Add `marker.offsetWidth` to this calculation because the position is calculated from top-left
+        if((getWindowXYSize()[0] - markerOffSetX) < (markerMaxWidth + marker.offsetWidth)) {            
+            offSetX = (markerMaxWidth + marker.offsetWidth) - (getWindowXYSize()[0] - markerOffSetX);
+        }
+        
+        map.panBy([offSetX, offSetY]);
+    }
+}
+
+      geojson.features.forEach((marker) => {
+        const svgMarker = renderMarker(marker);
+        // To get an actual DOM node instead of a string we append our marker to a dummy element and query it again with 'firstchild'. This way we retrieve a normal DOM node
+        const placeholder = document.createElement('div');
+        placeholder.innerHTML = svgMarker;
+        const el = placeholder.firstChild;
+
+        el.nextSibling.addEventListener('click', () => {
+            map.flyTo({
+                center: marker.geometry.coordinates,
+                zoom: 11,
+            });      
+        });
+
+        // Extend bounds with marker coordinates
+        bounds.extend(marker.geometry.coordinates);
+
+        markers.push(el.nextSibling);
+
+        new mapboxgl.Marker(el.nextSibling, {offset: [0, -30]})
+            .setLngLat(marker.geometry.coordinates)
+            .addTo(map);
+    });
+
+      map.on('load', (e) => {
+        setTimeout(() => {
+          map.fitBounds(bounds, {
+            padding: { top: 50, bottom: 50, left: 50, right: 50 },
+            easing(t) {
+              return t * (2 - t);
+            },
+          });
+        }, 300);
+      });
+
+      markers.forEach((marker) => {
+        const markerSVG = marker.querySelector('svg');
+        const markerIcon = marker.querySelector('.e-marker__icon');
+        const markerImage = marker.querySelector('.e-marker__image');
+        const markerText = marker.querySelector('.e-marker__text');
+
+        const markerRenderer = css(markerSVG, { enableHardwareAcceleration: false });
+        const iconRenderer = svg(markerIcon);
+        const imageRenderer = css(markerImage);
+        const textRenderer = css(markerText);
+
+        const markerGrowSize = 3;
+
+        const markerScale = physics({
+            from: 1,
+            to: markerGrowSize,
+            velocity: 20,
+            spring: 300,
+            friction: 0.8,
+            onUpdate: (x) => markerRenderer.set('scale', x),
+        });
+
+        const iconScale = tween({
+            from: 1,
+            to: 0,
+            duration: 300,
+            ease: easing.backIn,
+            onUpdate: (x) => iconRenderer.set('scale', x),
+        });
+
+        const imageScale = tween({
+            from: 0,
+            to: 1,
+            duration: 300,
+            ease: easing.backOut,
+            onUpdate: (x) => imageRenderer.set('scale', x),
+        });
+
+        const textToggle = tween({
+            from: 0,
+            to: 1,
+            duration: 300,
+            ease: easing.backOut,
+            onUpdate: (x) => textRenderer.set('opacity', x),
+        });
+
+        const hoverTimeline = timeline([
+            iconScale,
+            '0',
+            imageScale,
+            '-100',
+            textToggle,
+        ]);
+
+        marker.addEventListener('mouseenter', () => {
+            offSetMarker(marker, markerGrowSize, map);
+            markerScale.props.from = 1;
+            markerScale.props.to = markerGrowSize;
+            imageScale.props.playDirection = 1;
+            iconScale.props.playDirection = 1;
+            hoverTimeline.props.playDirection = 1;
+            textToggle.props.playDirection = 1; 
+            markerScale.start();
+            hoverTimeline.start();  
+        });
+
+        marker.addEventListener('mouseleave', () => {
+            markerScale.props.from = markerGrowSize;
+            markerScale.props.to = 1;
+            hoverTimeline.reverse();
+            hoverTimeline.start();
+            markerScale.start();
+        });
+
+    });
     },
   },
 };
@@ -69,5 +302,38 @@ export default {
   border: 2px solid #ccc;
   border-radius: 10px;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+.e-marker {
+  transform-origin: bottom center;
+  cursor: pointer;
+}
+
+.e-marker__marker {
+  fill: #fff;
+}
+
+.e-marker__icon {
+  fill: #fff;
+}
+
+.e-marker__image {
+  transform: scale(0);
+  transform-origin: 50% 50%;
+}
+
+.e-marker__circle {
+  display: none;
+}
+
+.e-marker__text {
+  font-size: 12px;
+  font-family: brandon-grotesque, sans-serif;
+  text-align: center;
+  opacity: 0;
+  color: #fff;
+  fill: #fff;
+  transform-origin: 50% 50%;
+  text-anchor: middle;
 }
 </style>
